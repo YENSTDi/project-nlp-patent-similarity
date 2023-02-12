cc1e3be3bd5fad06df27d107b6e96bc89520a56c
ryan <cglogc53112@gmail.com>
2023-02-12 20:32:02 +0800

    inference by gpu

--- PatentModel.py
+++ PatentModel.py
@@ -1,16 +1,21 @@
 import numpy as np
+import torch
 from transformers import AutoTokenizer
 from transformers import AutoModel
 
+
 class PatentModel():
-    def __init__(self, pretrained_name:str="anferico/bert-for-patents") -> None:
+    def __init__(self, pretrained_name:str="anferico/bert-for-patents", use_gpu=True) -> None:
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained_name)
         self.model = AutoModel.from_pretrained(pretrained_name)
+        self.device = "cuda:0" if torch.cuda.is_available() and use_gpu else "cpu"
+        self.model = self.model.to(self.device)
 
     def embedding(self, text:str):
         token = self.tokenizer(text, return_tensors='pt', max_length=512, truncation=True)
+        token = {k:v.to(self.device) for k, v in token.items()}
         output = self.model(**token).last_hidden_state[:, 0, :]
-        return output.detach().numpy()
+        return output.cpu().detach().numpy()
     
     def embedding_by_concat(self, text:str, max_lenght:int=510):
         text_list = self.split_text_by_words(text, max_lenght)
