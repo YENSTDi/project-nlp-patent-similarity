7a24288864b2d1b3ba2422494a6336bc8409945a
ryan <cglogc53112@gmail.com>
2023-03-20 21:22:12 +0800

    get data

--- Task1.py
+++ Task1.py
@@ -1,29 +1,73 @@
 import os
-import pandas as pd
-from tqdm import tqdm
 from glob import glob
 
-class Task1():
-    def __init__(self, task1_data:str='./data/A61B005.xlsx', task1_folder:str="./data/A61B005專利_1996-2020/*") -> None:
-        self.task1_data = task1_data
-        self.task1_folder = task1_folder
-        self.data = self.get_data()
+import pandas as pd
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+
+class Data():
+    def __init__(self,
+                 other_data: str = './data-2/other.xlsx',
+                 other_folder: str = "./data-2/A61B005專利_1996-2020/*",
+                 apple_data: str = "./data-2/Apple.xlsx",
+                 apple_file: str = "./data-2/Apple_Full text.XML"
+                 ) -> None:
+        self.other_data = other_data
+        self.other_folder = other_folder
+        self.apple_data = apple_data
+        self.apple_file = apple_file
+
         pass
-    
-    def get_data(self):
-        if os.path.exists(self.task1_data):
-            return pd.read_excel(self.task1_data)
+
+    def other(self):
+        if os.path.exists(self.other_data):
+            print("other company file exist.")
+            return pd.read_excel(self.other_data)
         else:
             tmp_data = []
-            for i in tqdm(glob(self.task1_folder)):
+            for i in tqdm(glob(self.other_folder)):
                 data = pd.read_excel(i)
                 tmp_data.append(data)
             data = pd.concat(tmp_data)
-            data.to_excel(self.task1_data)
+            data.to_excel(self.other_data, index=False)
             return data
+
     def clean(self):
         pass
 
-if __name__=="__main__":
-    task1_data = Task1().get_data()
+    # @staticmethod
+    def apple(self):
+        if os.path.exists(self.apple_data):
+            print("apple company file exist.")
+            return pd.read_excel(self.apple_data)
+
+        with open(self.apple_file, 'r') as f:
+            data = f.read()
+
+        soup = BeautifulSoup(data, "html.parser")
+        apple_case = soup.findAll("doc")
+        result = []
+        for i in apple_case:
+            uid = i.find("uuid").text
+            content1 = i.find("str", {"name": "權利要求"})
+            content2 = i.find("str", {"name": "說明書"})
+
+            content1 = content1.text if content1 is not None else ""
+            content2 = content2.text if content2 is not None else ""
+
+            tmp_df = pd.DataFrame({
+                "uid": [uid],
+                "content": [content1+" "+content2]
+            })
+            result.append(tmp_df)
+
+        result = pd.concat(result)
+        result.to_excel(self.apple_data, index=False)
+        return result
+        pass
+
+
+if __name__ == "__main__":
+    task1_data = Data().other()
     print(len(task1_data))
